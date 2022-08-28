42c826ca11e9fbc9fa5d47073e23b7cc20cdaacf
Chutato1108 <112267923+Chutato1108@users.noreply.github.com>
2022-08-28 12:37:39 -0500

    no le muevan plox
    no le muevan

---
+++ Museo/Assets/Scripts/TP_Object.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoadinator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void GoToscene(string sceneName){
+        SceneManager.LoadScene(sceneName);
+    }
+}
