df020d850e3ec803826afcb7653131bfbb8f2592
Chutato1108 <112267923+Chutato1108@users.noreply.github.com>
2022-08-28 12:48:15 -0500

    tampoco le muevan xd

---
+++ Museo/Assets/Scripts/TP_Escena.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TP_Escena : MonoBehaviour
+{
+    public string sceneName;
+    // Start is called before the first frame update
+    
+    // Update is called once per frame
+    void OnTriggerEnter(Collider Other)
+    {
+        if(Other.tag == "Player"){
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
